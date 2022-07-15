25cd38c2335ebc5d29f53ebee5beb96fc5853233
ПК <GIS_1974@tut.by>
2022-07-15 10:21:26 +0300

    HomeWork2

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleAppFactorial1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pleas, enter number to calv factorial");
+            int nFactorial = int.Parse(Console.ReadLine());
+            int fact = 1; 
+            for (int i = 2; i <= nFactorial; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine(fact);
+        }
+    }
+}
